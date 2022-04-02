7e4e897e003adb649387bcb714e86a9be9431e0d
Saud Khan <khansaud3551@gmail.com>
2022-04-02 15:43:16 +0500

    #content change

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,16 +7,26 @@ function Card() {
         <div className="col-md-7 size">
           <h1 className="text-start my-3 my-md-1">GROUND ZERO : </h1>
           <p className="">
-            Once upon a blockchain many many epochs from now in an ecosystem
-            many blockchains away from the Cardano universe, there was a large
-            asteroid that crashed into the Apocalypticats planet decimating
-            Almost all life, covering the planet with thick fog ,complete
-            silence, and a presence of death and blood
+            Once upon a blockchain, many many epochs from now.
+            <br /> In an ecosystem, a million blockchains away from the Cardano
+            universe, there was a bountiful planet full of intelligent life. The
+            inhabitants Are now known as the Apocalypticats. A once jolly and
+            peaceful colony of cats always just hanging out in the bunker,
+            helping each other out. Unfortunately, their lives were about to be
+            changed forever.
           </p>
-          <h1 className="text-start my-3 my-md-1">BEFORE IMPACT : </h1>
+          <p>
+            One calm starry night, while most of the cats were asleep, a large
+            asteroid crashed into the Apocalypticats planet. It never showed up
+            on their radar. Not till it was too late, The Apocalypticat king had
+            no choice but to take 4444 apocalypticats and lead them underground
+            into the bunker to wait out the hail of fire.
+          </p>
+
           <p>
             The Apocalypticat king had no choice but to take 4444 apocalypticats
-            and lead them underground into the bunker to wait out the blast…………{" "}
+            and lead them underground into the bunker to wait out the hail of
+            fire.
           </p>
         </div>
         <div className="col-md-5 mt-5 ">
@@ -24,20 +34,27 @@ function Card() {
         </div>
         <h1 className="text-start my-4 my-md-1">POST IMPACT : </h1>
         <p className="mb-3 mb-md-0 size">
-          After impact the king tasked 3333 elite Apocalypticat warriors to
-          escape the destroyed planet, To find a way to save the rest in the
-          bunker , the remaining 1111 were tasked to stay and hold down the
-          bunker….
+          Covered in a thick fog, complete silence, and a presence of death and
+          blood, the once vibrant planet was burning and losing almost all signs
+          of life rapidly.
+        </p>
+        <p className="size">
+          The king had tasked 3333 elite Apocalypticat warriors to escape the
+          destroyed planet and do whatever it took to save the rest of the cats
+          in the bunker. The remaining 1111 apocalypticats were tasked to stay
+          and salvage there planet anddiscover whatever remained
         </p>
         <h1 className="font_large text-center my-3 my-md-5">
           Apocalypticat lore
         </h1>
         <div className="col-12">
           <p className="size text-center">
-            It is unknown if the 1111 are still alive and how the 3333
-            Apocalypticats escaped the planet and why they chose to go back in
-            time into the cardano ecosystem, All we know forsure is that they
-            are set out to save there kin and their homeland,
+            It is unknown if the 1111 Apocalypticats are still alive back home,
+            How the 3333 Apocalypticats escaped the planet Or Why they chose to
+            go back in time into the Cardano ecosystem, No one can answer these
+            questions for sure. We can only tell you TWO things. They are out to
+            save their homeland and They are out for answers….. Monthly comic
+            strip coming soon,
           </p>
         </div>
       </div>
